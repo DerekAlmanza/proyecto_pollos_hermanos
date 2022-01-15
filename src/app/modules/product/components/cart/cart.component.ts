39249b29c7e818850bd3378c3696a414b2434526
cheo0 <eliseo_0@ciencias.unam.mx>
2022-01-14 21:58:09 -0600

    feature/cart Se implemento la funcionalidad del boton 'Eliminar' del carrito.
    Modulo: Cart
    Responsable: cheo0

--- src/app/modules/product/components/cart/cart.component.ts
+++ src/app/modules/product/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import Swal from "sweetalert2";
 import { Cart } from "../../_model/cart";
 import { CartService } from "../../_service/cart.service";
 
@@ -25,5 +26,48 @@ export class CartComponent implements OnInit {
       },
       (error) => console.error(error)
     );
+  }
+
+  /** Metodo para eliminar un elemento del carrito. */
+  removeFromCart(id_cart: number) {
+    Swal.fire({
+      title: '¿Seguro que deseas eliminar el producto?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, elimina',
+      cancelButtonText: 'Cancelar'
+    }).then(result => {
+      if (result.isConfirmed) {
+        this.cartService.removeFromCart(id_cart).subscribe(
+          _ => {
+            this.getCart();
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: 'Eliminación exitosa!',
+              showConfirmButton: false,
+              timer: 2000
+            });
+          },
+          err => {
+            console.error(err);
+            this.failedAlert();
+          }
+        )
+      }
+    })
+  }
+
+  /**
+  * Alerta en caso de que algo salga mal.
+  */
+  failedAlert() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Algo salió mal ):',
+    })
   }
 }
