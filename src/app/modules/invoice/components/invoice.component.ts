aaf9eb86c552212b27a309b1db1d795a706bcd05
Dicter Rosas <dicteraulad@ciencias.unam.mx>
2022-01-16 02:16:14 -0600

    Ver detalle de factura. Extrae la información y la muestra en terminal pero no en el modal Responsable: Dicter Rosas.

--- src/app/modules/invoice/components/invoice.component.ts
+++ src/app/modules/invoice/components/invoice.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Form, FormBuilder } from '@angular/forms';
 import { InvoiceService } from '../_service/invoice.service';
 import { Item } from '../_model/item';
 import { Invoice } from '../_model/invoice';
 import { Router } from '@angular/router';
+
+declare var $:any;
 
 @Component({
   selector: 'app-invoice',
@@ -15,11 +17,22 @@ export class InvoiceComponent implements OnInit {
   invoices: Invoice[] = [];
   invoice: Invoice = new Invoice();
   items: Item[] = [];
+  item: Item = new Item();
   submitted = false;
+  formulario = this.formBuilder.group({
+    id_invoice: [''],
+    rfc: [''],
+    subtotal: [''],
+    taxes: [''],
+    total: [''],
+    created_at: ['']
+})
+
 
   constructor(
     private invoice_service: InvoiceService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   getInvoice(){
@@ -33,8 +46,21 @@ export class InvoiceComponent implements OnInit {
     this.getInvoice();
   }
 
-  invoiceDetail(rd_invoice:string){
+  invoiceDetail(id_invoice:number){
     console.log("aqui redirigimos al detalle de la factura");
+    this.invoice_service.getItems(id_invoice).subscribe(
+      res => {
+        this.items = res;
+        console.log(res);
+        $('#invoiceModal').modal('show');
+      },
+      err => console.log(err)
+    )
+  }
+
+  closeModal(){
+    $('#invoiceModal').modal('hide');
+    this.submitted = false;
   }
 
 }
